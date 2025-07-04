4fa4790682ce32e9fe0b8b92949eefc056d89606
Yedukondalu Cheeraboina <ykyadav.pra@gmail.com>
2025-07-04 10:45:36 +0530

    Delete 10. Functions/1. Functions directory

--- 10. Functions/1. Functions/ValueTypeEx2.py
+++
@@ -1,8 +0,0 @@
-#ValueTypeEx2.py
-def dispvalue(x:str):
-    print("Given data={}".format(x))
-    print("Upper Case data=",x.upper())
-    print("lower Case data=", x.lower())
-
-#main Program
-dispvalue("Python Is an Oop Lang")
